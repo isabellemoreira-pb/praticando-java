46d0505d3e9f326625575bd000794bc7dd4c6af0
Isabelle Moreira <imaciel@uolinc.com>
2026-05-18 14:33:52 -0300

    For e While 1.1

--- src/ForEWhile/Exercicio10.java
+++ src/ForEWhile/Exercicio10.java
@@ -1,4 +1,45 @@
 package ForEWhile;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exercicio10 {
+    public static void main(String[] args) {
+        ArrayList<String> listaDeConvidados = new ArrayList<>();
+        String nome;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Insira o nome do convidado (ou \"ver\" para visitar a lista): ");
+            nome = leitura.nextLine().trim();
+
+            if(nome.equalsIgnoreCase("sair")){
+                System.out.println("Programa finalizado.");
+                break;
+            }
+
+            if (nome.equalsIgnoreCase("ver")){
+                System.out.println("Lista atualizada de convidados: " + listaDeConvidados);
+                continue;
+            }
+
+            boolean existe = false;
+
+            for (String convidados : listaDeConvidados){
+                if (convidados.toLowerCase().equalsIgnoreCase(nome)){
+                    existe = true;
+                    break;
+                }
+            }
+
+            if(existe){
+                System.out.println("O nome " + nome + "já está na lista.");
+            } else {
+                listaDeConvidados.add(nome);
+                System.out.println(nome + " foi adicionado à lista de convidados.");
+            }
+
+        }
+    }
 }
--- src/ForEWhile/Exercicio8.java
+++ src/ForEWhile/Exercicio8.java
@@ -1,4 +1,38 @@
 package ForEWhile;
 
+import java.util.Scanner;
+
 public class Exercicio8 {
+    public static void main(String[] args) {
+        int numero;
+        int positivos = 0;
+        int negativos = 0;
+        String entrada;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Insira um número (ou fim para encerrar: )");
+            entrada = leitura.nextLine();
+
+            if (entrada.equalsIgnoreCase("fim")) {
+                System.out.println("Programa encerrado!");
+                break;
+            }
+
+            numero = Integer.parseInt(entrada);
+
+            if (numero > 0) {
+                positivos++;
+            } else if (numero < 0) {
+                negativos++;
+            } else {
+                System.out.println(" 0 é um número neutro.");
+            }
+        }
+
+        System.out.println("Números positivos: " + positivos);
+        System.out.println("Números positivos: " + negativos);
+        leitura.close();
+    }
 }
